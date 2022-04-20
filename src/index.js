3242352f7d2c76f87321feb08ead8dce1c0a5d63
shaileshpandit3 <shaileshpandit85@gmail.com>
2022-04-20 09:55:29 +0530

    final

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ app.use(multer().any())
 
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://shailesh123:rYbeOdoWZtY9NdKU@cluster0.e1ege.mongodb.net/group22Database-DB?retryWrites=true&w=majority", {useNewUrlParser: true})
+mongoose.connect("mongodb+srv://shailesh123:rYbeOdoWZtY9NdKU@cluster0.e1ege.mongodb.net/group22Database-DB?retryWrites=true&w=majority", { useNewUrlParser: true })
     .then(() => console.log('mongodb running and connected'))
     .catch(err => console.log(err))
 
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-	console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
